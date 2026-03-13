7a984c506857a9eda406f3121fcff0dee2a2323b
Mustafa <mustafaarifin88@admin.sd.belajar.id>
2026-03-13 12:32:39 +0700

    Auto Save

---
+++ app/Filters/AdminFilter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\Filters\FilterInterface;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class AdminFilter implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        if (!session()->get('role')) {
+            return redirect()->to(base_url('auth'));
+        }
+
+        if (session()->get('role') !== 'admin') {
+            return redirect()->to(base_url('kasir/dashboard'));
+        }
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+    }
+}
